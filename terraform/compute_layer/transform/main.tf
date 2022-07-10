73e6346c664bad3ad5d22e8b5bc16598a40c38ad
Fernando Zepeda <fernando.zepeda@qurix.tech>
2022-07-10 23:35:56 +0200

    add storage for transform

--- terraform/compute_layer/transform/main.tf
+++ terraform/compute_layer/transform/main.tf
@@ -1,3 +1,14 @@
+resource "azurerm_storage_account" "sa" {
+  name                      = "sa${var.environment}ingest"
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  allow_blob_public_access  = false
+  enable_https_traffic_only = true
+}
+
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-${var.environment}-transform-${var.use_case}"
   resource_group_name = var.resource_group_name
