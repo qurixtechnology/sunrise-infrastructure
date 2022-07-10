62ea6767241f54e97a1f98fb0b33fbe71905f920
Fernando Zepeda <fernando.zepeda@qurix.tech>
2022-07-10 18:42:11 +0200

    inital commit

---
+++ terraform/storage_layer/create_azure_ad_logins_db.sql
@@ -0,0 +1,13 @@
+-- Create Azure AD users: needed after deployment with Terraform
+CREATE USER [fernando.zepeda@qurix.tech] FROM EXTERNAL PROVIDER;
+
+-- Grant roles
+ALTER ROLE db_datareader ADD MEMBER [fernando.zepeda@qurix.tech];
+ALTER ROLE db_datawriter ADD MEMBER [fernando.zepeda@qurix.tech];
+ALTER ROLE db_ddladmin ADD MEMBER [fernando.zepeda@qurix.tech];
+
+--Grant Database Permissions
+GRANT VIEW DATABASE STATE TO [fernando.zepeda@qurix.tech]
+
+-- Add Server Role to admin
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [konkevych@qurix.tech];
